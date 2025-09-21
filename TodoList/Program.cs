aa40df55ab3d4e98a6f035d46728dbf5db6a640b
Георгий Шелепов <shelepov15945@gmail.com>
2025-09-21 09:22:14 +0300

    Todolist

---
+++ TodoList/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Jobcompleteby
+{
+
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Работу выполнили Шелепов и Кузьменко");
+
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Введите год рождения");
+            string yo = Console.ReadLine();
+
+            int yeartoday = 2025;
+            int yo2 = int.Parse(yo);
+            int age = yeartoday - yo2;
+
+            Console.WriteLine("Добавлен пользователь " + name + " " + surname + " Возраст - " + age);
+
+        }
+
+    }
+}
