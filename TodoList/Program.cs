0c0f67e8016836e59356e2af0383b56cf325668c
Angelina <degtyaryova.angilina@yandex.ru>
2025-09-25 17:03:28 +0300

    Data request

---
+++ TodoList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запрашиваем данные у пользователя
+        Console.Write("Введите имя: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Введите фамилию: ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("Введите год рождения: ");
+        string yearInput = Console.ReadLine();
+
+    }
+}
