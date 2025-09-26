cbfbab032e9cc015b1f0ba363e05605a5622b12b
Дмитрий Бурнашов <maybe.dimka@gmail.com>
2025-09-26 16:15:02 +0300

    Добавлено консольное приложение ToddList

---
+++ Todolist/Todolist/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ToddList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работу выполнили Бурнашов и Хазиев");
+
+            Console.Write("Введите имя: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Введите фамилию: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введите год рождения: ");
+            string yearInput = Console.ReadLine();
+
+            int birthYear = int.Parse(yearInput);
+            int currentYear = DateTime.Now.Year;
+            int age = currentYear - birthYear;
+
+            Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст – {age}");
+        }
+    }
+}
