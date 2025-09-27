c000a4e0ab2408f049508a74ec116417cdaacad6
Артем Бешкеев <11071980ir@gmail.com>
2025-09-27 18:05:26 +0300

    Lesson 2 закоммитили все файлы во второй практической

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 
-Console.Write("Введите имя: ");
-string name = Console.ReadLine();
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Работу выполнили Бешкеев и Печерский");
 
-Console.Write("Введите фамилию: ");
-string surname = Console.ReadLine();
+        Console.Write("Введите имя: ");
+        string name = Console.ReadLine();
 
-Console.Write("Введите год рождения: ");
-string birthYearString = Console.ReadLine();
+        Console.Write("Введите фамилию: ");
+        string surname = Console.ReadLine();
 
-int birthYear = int.Parse(birthYearString);
-int currentYear = DateTime.Now.Year;
-int age = currentYear - birthYear;
+        Console.Write("Введите год рождения: ");
+        string birthYearString = Console.ReadLine();
 
-Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
+      
+        int birthYear = int.Parse(birthYearString);
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthYear;
+
+       
+        Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
+    }
+}
