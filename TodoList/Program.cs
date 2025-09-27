00b84c355cd4de6954158a56246c4a3f0ccbb6e4
Aleks55466 <aleksejbagdanovic761@gmail.com>
2025-09-27 19:15:48 +0300

    Lesson 2 Добавили программу

---
+++ TodoList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Работу выполнили Прокопенко и Морозов");
+
+        Console.Write("Введите имя: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Введите фамилию: ");
+        string surname = Console.ReadLine();
+
+        Console.Write("Введите год рождения: ");
+        string birthYearString = Console.ReadLine();
+
+
+        int birthYear = int.Parse(birthYearString);
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthYear;
+
+
+        Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
+    }
+}
