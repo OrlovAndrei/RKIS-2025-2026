ff56b6c2a707ee810caf96f0b8402dde7b6c7882
Kotyaraa5 <mckrakazyabra@gmail.com>
2025-09-27 10:17:51 +0300

    Program.cs

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -1,13 +1,12 @@
 ﻿Console.WriteLine("Работу выполнили: Амелина Яна и Кабанова Арина");
 Console.WriteLine("ВВедите свое имя");
-    String name = Console.ReadLine();
+String name = Console.ReadLine();
 Console.WriteLine("Введите свою фамилию");
-    String Surname = Console.ReadLine();
+String Surname = Console.ReadLine();
 Console.WriteLine("ВВедите свою год рождения");
-    String date1 = Console.ReadLine();
+String date1 = Console.ReadLine();
 int date2 = int .Parse(date1);
 int date3 = 2025;
 int age = date3 - date2; 
-
 Console.WriteLine("Добавлен пользователь " + name + " " + Surname + ", Возраст " + age);
 
