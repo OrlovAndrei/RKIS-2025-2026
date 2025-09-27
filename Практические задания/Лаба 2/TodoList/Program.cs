43f34260daa741eea4880344f5b53d89b50ab513
mikhail-petrenko <mikhail.petrenko.work@gmail.com>
2025-09-27 08:47:11 +0300

    Выполнение практической работы к Лабораторной работе №2

---
+++ Практические задания/Лаба 2/TodoList/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TodoList
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Введите ваше имя: "); 
+            string firstName = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введите ваш год рождения: ");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст - {DateTime.Now.Year - year}");
+        }
+    }
+}
