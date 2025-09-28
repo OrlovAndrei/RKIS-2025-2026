d4b8c572b004a303e809b332f50055629cf18c88
MrIce <phull.akk.1988@gmail.com>
2025-09-28 17:20:09 +0300

    lesson 2: add TodoList

---
+++ TodoList/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace TodoList
+{
+	class MainClass
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Работу выполнили: Вдовиченко и Кравец");
+			Console.Write("Введите ваше имя: ");
+			string name = Console.ReadLine();
+			Console.Write("Введите вашу фамилию: ");
+			string surname = Console.ReadLine();
+
+			Console.Write("Введите ваш год рождения: ");
+			int year = int.Parse(Console.ReadLine());
+
+			string text = $"Добавлен пользователь {name} {surname}, возраст - {DateTime.Now.Year - year}";
+			Console.WriteLine(text);
+		}
+	}
+}
