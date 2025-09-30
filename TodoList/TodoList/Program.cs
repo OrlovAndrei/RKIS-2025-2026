bc4b71edd7c77731d5993c9fbcfa4cc342c2351b
Амелина Яна <mckrakazyabra@gmail.com>
2025-09-30 11:07:27 +0300

    Lesson 3: command: help, profile, add, view

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -1,5 +1,7 @@
 
-﻿internal class Program
+using System.Threading.Channels;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -17,8 +19,45 @@
         int arrayLength = 2;
         string[] todos = new string[arrayLength];
         bool isOpen = true;
-  
+        while (isOpen)
+        {
+            string userCommand = "";
+            Console.WriteLine("Введите команду:\nдля помощи напиши команду help");
+            userCommand = Console.ReadLine();
+            switch (userCommand)
+            {
+                case "help":
+                    Console.WriteLine("help - выводит список всех доступных команд\nprofile - выводит ваши данные\nadd - добавляет новую задачу\nview - просмотр задач\nexit - выйти");
+                    break;
+                case "profile":
+                    Console.WriteLine("Пользователь: " + name + " " + Surname + ", Возраст " + age);
+                    break;
+                case "add":
+                    for (int i = 0; i < arrayLength; i++)
+                    {
+                        if (string.IsNullOrEmpty(todos[i]))
+                        {
+                            Console.WriteLine("Напишите задачу которую необходимо добавить");
+                            todos[i] = Console.ReadLine();
+                            break;
+                        }
+                    }
+                    break;
+                case "view":
+                    for (int i = 0;i < arrayLength; i++)
+                    {
+                        if (!string.IsNullOrEmpty(todos[i]))
+                            Console.WriteLine(todos[i]);
+                    
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Неправильно введена команда");
+                    break;
+            }
 
 
+
+        }
     }
 }
