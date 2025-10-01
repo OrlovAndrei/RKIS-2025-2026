323fcb8703e244db421491d5e5da65bea1ef75e5
Артем Бешкеев <11071980ir@gmail.com>
2025-10-02 00:21:02 +0300

    создали массив строк Todos и сделали бесконечный цикл

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -3,10 +3,13 @@
 class Program
 {
     static void Main(string[] args)
-    { 
+    {
+        string name = "Артем";
+        string surname = "Бешкеев";
+        int birthYear = 2007;
+
         string[] todos = new string[2];
-        
-        int todoCount = 0;
+        int todoCount = 0; 
 
         Console.WriteLine("Введите команду (help - список команд):");
 
