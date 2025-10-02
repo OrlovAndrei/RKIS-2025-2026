3e05a21f5ee173c3c2c208b8ce86c36c62b528b0
Angelina <degtyaryova.angilina@yandex.ru>
2025-10-02 21:27:18 +0300

    Create arrey 2 elements

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -2,7 +2,12 @@
 
 class Program
 {
-    static void Main()
+        // Массив в 2 элемента
+        static string[] todos = new string[2];
+        static string firstName = "";
+        static string lastName = "";
+        static int birthYear = 0;
+        static void Main()
     {
         // Запрос данных
         Console.Write("Введите имя: ");
