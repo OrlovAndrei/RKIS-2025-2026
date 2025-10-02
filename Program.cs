27e24905f918850a82340c3221158f2786289b61
Turishev <139759816+MOPOZKA@users.noreply.github.com>
2025-10-02 18:27:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("выполнил работу Турищев Иван");
+        Console.ReadLine();
+    }
+}
+class EnteringInteger {
+    public void lol()
+    {
+        string res, txt;
+        int year = DateTime.Now.Year, age = -1, born = -1;
+        res = Interaction.InputBox("в каком году вы поделись ?", "год рождения");
+        if (int.TryParse(res, out born))
+        //born = Int32.Parse(res);
+        age = year - born;
+        txt = $"ну ты старый, {age} лет";
+        MessageBox.Show(txt, "возраст");
+    }
+}
+
