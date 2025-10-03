f2adf7c2696100244f5e5d12c318b61a1608c5ec
Aleks55466 <aleksejbagdanovic761@gmail.com>
2025-10-03 21:02:13 +0300

    Добавил  help

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -17,3 +17,14 @@ class Program
         {
             Console.Write("> ");
             string input = Console.ReadLine();
+            
+          if (input == "help")
+            {
+                Console.WriteLine("Доступные команды:");
+                Console.WriteLine("help - показать список команд");
+                Console.WriteLine("profile - показать наши данные");
+                Console.WriteLine("add "текст задачи" - добавить задачу");
+                Console.WriteLine("view - показать все задания");
+                Console.WriteLine("exit - выйти из программы");
+                continue;
+            }
