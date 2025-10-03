f2ac7c830e2969b5687985c203e624a87a560c40
Aleks55466 <aleksejbagdanovic761@gmail.com>
2025-10-03 22:04:26 +0300

    Сделал push в fork

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -9,7 +9,7 @@ class Program
         int birthYear = 2007;
 
         string[] todos = new string[2];
-        int todoCount = 0; 
+        int todoCount = 0;
 
         Console.WriteLine("Добро пожаловать, введите help:");
 
@@ -17,17 +17,18 @@ class Program
         {
             Console.Write("> ");
             string input = Console.ReadLine();
-            
-          if (input == "help")
+
+            if (input == "help")
             {
                 Console.WriteLine("Доступные команды:");
                 Console.WriteLine("help - показать список команд");
                 Console.WriteLine("profile - показать наши данные");
-                Console.WriteLine("add "текст задачи" - добавить задачу");
+                Console.WriteLine("add \"текст задачи\" - добавить задачу");
                 Console.WriteLine("view - показать все задания");
                 Console.WriteLine("exit - выйти из программы");
                 continue;
             }
+
             if (input == "profile")
             {
                 Console.WriteLine($"{name} {surname}, {birthYear}");
@@ -36,56 +37,57 @@ class Program
 
             if (input.StartsWith("add "))
             {
-                string[] parts = input.Split(' ');
-                if (parts.Length >= 2 && parts[0] == "add")
-                {
-                    string taskPart = string.Join(" ", parts.Skip(1));
-                    if (taskPart.StartsWith(""") && taskPart.EndsWith(""") && taskPart.Length > 2)
-                    {
-                        string task = taskPart.Substring(1, taskPart.Length - 2);
+                int firstQuoteIndex = input.IndexOf('\"');
+                int lastQuoteIndex = input.LastIndexOf('\"');
 
-                        if (todoCount == todos.Length)
-                        {
-                            string[] newTodos = new string[todos.Length * 2];
-                            for (int i = 0; i < todos.Length; i++)
-                            {
-                                newTodos[i] = todos[i];
-                            }
-                            todos = newTodos;
-                        }
-
-                        todos[todoCount] = task;
-                        todoCount++;
-
-                        Console.WriteLine("Задача была добавлена.");
-                    }
-                    else
-                    {
-                       Console.WriteLine("Неправильно введена команда. Используйте add "текст задачи"");
-                    }
-                    continue;
-                }
-                
-                if (input == "view")
+                if (firstQuoteIndex != -1 && lastQuoteIndex != -1 && lastQuoteIndex > firstQuoteIndex)
                 {
-                    if (todoCount == 0)
+                    string task = input.Substring(firstQuoteIndex + 1, lastQuoteIndex - firstQuoteIndex - 1);
+
+                    if (todoCount == todos.Length)
                     {
-                        Console.WriteLine("Задач нет.");
+                        string[] newTodos = new string[todos.Length * 2];
+                        Array.Copy(todos, newTodos, todos.Length);
+                        todos = newTodos;
                     }
-                    else
-                    {
-                        Console.WriteLine("Список задач:");
-                        for (int i = 0; i < todoCount; i++)
-                        {
-                            if (!string.IsNullOrEmpty(todos[i]))
-                                Console.WriteLine($"{i + 1}. {todos[i]}");
-                        }
-                    }
-                    continue;
+
+                    todos[todoCount] = task;
+                    todoCount++;
+
+                    Console.WriteLine("Задача была добавлена.");
                 }
-                
-                if (input == "exit")
+                else
                 {
-                    Console.WriteLine("Выход из программы.");
-                    break;
+                    Console.WriteLine("Неправильный формат команды. Используйте: add \"текст задачи\"");
                 }
+                continue;
+            }
+
+            if (input == "view")
+            {
+                if (todoCount == 0)
+                {
+                    Console.WriteLine("Задач нет.");
+                }
+                else
+                {
+                    Console.WriteLine("Список задач:");
+                    for (int i = 0; i < todoCount; i++)
+                    {
+                        if (!string.IsNullOrEmpty(todos[i]))
+                            Console.WriteLine($"{i + 1}. {todos[i]}");
+                    }
+                }
+                continue;
+            }
+
+            if (input == "exit")
+            {
+                Console.WriteLine("Выход из программы.");
+                break;
+            }
+
+            Console.WriteLine("Неизвестная команда. Введите help для списка команд.");
+        }
+    }
+}
