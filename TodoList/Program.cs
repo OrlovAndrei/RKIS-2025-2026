3e8fb0563a726f9cac14ee94d67e7e51f9836d76
kipish89 <wpam986@gmail.com>
2025-10-04 15:47:48 +0300

    lesson 3 error

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,85 +1,81 @@
 ﻿Console.WriteLine("Работу выполнили Андрей и Роман и Петр");
 Console.WriteLine("Введите Имя");
-string name = Console.ReadLine();
+string Name = Console.ReadLine();
 Console.WriteLine("Введите Фамилию");
-string surname = Console.ReadLine();
+string Surname = Console.ReadLine();
 Console.WriteLine("Введите дату рождения");
-int date = Convert.ToInt32(Console.ReadLine());
-int year1 = 2025;
-int finalnumber;
-finalnumber = year1 - date;
-Console.WriteLine("Добавлен пользователь"); Console.WriteLine(name); Console.WriteLine(surname); Console.WriteLine(finalnumber);
-
+int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+int CurrentYear = 2025;
+int CurrentNumber;
+CurrentNumber = CurrentYear - YearOfBirth;
+Console.WriteLine($"\nДобавлен пользователь: Имя: {Name}, Фамилия: {Surname}, Возраст: {CurrentNumber}"); 
 string[] todos = new string[3];
 while (true)
 {
     Console.WriteLine("Введите команду: ");
     string command = Console.ReadLine();
-    if (command == "help")
+    switch (command)
     {
-        Console.WriteLine("Доступные команды:");
-        Console.WriteLine("help - показать все команды");
-        Console.WriteLine("profile - показать профиль");
-        Console.WriteLine("add \"задача\" - добавить задачу");
-        Console.WriteLine("view - показать все задачи");
-        Console.WriteLine("exit - выйти из программы");
-    }
-    if (command == "profile")
-    {
-        string name1 = name;
-        string surname1 = surname;
-        int year = date;
-
-        Console.WriteLine($"{name1} {surname1}, {year}");
-    }
-
-    if (command.StartsWith("add"))
-    {
-        string task = command.Substring(4).Trim().Trim('"');
-        for (int i = 0; i < todos.Length; i++)
-        {
-            if (todos[i] == null)
+        case "help":
+            Console.WriteLine("Доступные команды:");
+            Console.WriteLine("help - показать все команды");
+            Console.WriteLine("profile - показать профиль");
+            Console.WriteLine("add \"задача\" - добавить задачу");
+            Console.WriteLine("view - показать все задачи");
+            Console.WriteLine("exit - выйти из программы");
+            break;
+        case "profile":
+            Console.WriteLine($"{Name} {Surname}, {YearOfBirth}");
+            break;
+        case "view":
+            Console.WriteLine("Список задач:");
+            bool hasTasks = false;
+            for (int i = 0; i < todos.Length; i++)
             {
-                todos[i] = task;
-                Console.WriteLine($"Задача добавлена: {task}");
-                break;
-            }
-            if (i == todos.Length - 1)
-            {
-                string[] newTodos = new string[todos.Length * 2];
-                for (int j = 0; j < todos.Length; j++)
+                if (!string.IsNullOrEmpty(todos[i]))
                 {
-                    newTodos[j] = todos[j];
+                    Console.WriteLine($"{i + 1}. {todos[i]}");
+                    hasTasks = true;
                 }
-                todos = newTodos;
-                Console.WriteLine("Массив расширен!");
-                todos[i + 1] = task;
-                Console.WriteLine($"Задача добавлена: {task}");
-                break;
             }
-        }
-    }
-    if (command == "view")
-    {
-        Console.WriteLine("Список задач:");
-        bool hasTasks = false;
-
-        for (int i = 0; i < todos.Length; i++)
-        {
-            if (!string.IsNullOrEmpty(todos[i]))
+            if (!hasTasks)
+            {       
+                Console.WriteLine("Задач нет!");
+            }
+            break;
+        case "exit":
+            Console.WriteLine("До свидания");
+            return;
+            if (command.StartsWith("add"))
             {
-                Console.WriteLine($"{i + 1}. {todos[i]}");
-                hasTasks = true;
-            }
-        }
-        if (!hasTasks)
+                string task = command.Substring(4).Trim(' ', '"');
+                for (int i = 0; i < todos.Length; i++)
+                {
+                    if (string.IsNullOrEmpty(todos[i]))
+                    {
+                        todos[i] = task;
+                        Console.WriteLine($"Задача добавлена: {task}");
+                        break;
+                    }
+                    if (i == todos.Length - 1)
+                    {
+                        string[] newTodos = new string[todos.Length * 2];
+                        for (int j = 0; j < todos.Length; j++)
+                    {
+                        newTodos[j] = todos[j];
+                    }
+                    todos = newTodos;
+                    Console.WriteLine("Массив расширен!");
+                    todos[i + 1] = task;
+                    Console.WriteLine($"Задача добавлена: {task}");
+                    break;
+                    }
+                }
+               }
+            if (command == "exit")
         {
-            Console.WriteLine("Задач нет!");
-        }
-    }
-    if (command == "exit")
-    {
         Console.WriteLine("До свидания!");
         break;
+        }
     }
 }
