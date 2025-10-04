61a505a62aa0c5eb9a7bd66fb900d29cd962a0b0
anastaflex <anastasiakabacek6@gmail.com>
2025-10-04 13:32:28 +0300

    lesson 3: adding commands(add, view)

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -16,7 +16,9 @@
             
             Console.WriteLine("Добавлен пользователь " + name + " " + surname + ", возраст - " + age);
             
-
+            string[] todos = new string[2];
+            int index = 0;
+            
             while (true)
             {
                 Console.WriteLine("Введите команду:");
@@ -35,13 +37,45 @@
                 {
                     Console.WriteLine(name + " " + surname + " - " + age);
                 }
-
                 else if (command == "exit")
                 {
                     Console.WriteLine("Выход из программы.");
                     break;
                 }
-                
+                else if (command.StartsWith("add "))
+                {
+                    string task = command.Split("add ")[1];
+                    if (index == todos.Length)
+                    {
+                        string[] newTodos = new string[todos.Length*2];
+                        for (int i = 0; i < todos.Length; i++)
+                        {
+                            newTodos[i] = todos[i];
+                        }
+                        
+                        todos = newTodos;
+                    }
+
+                    todos[index] = task;
+                    index = index + 1;
+                    
+                    Console.WriteLine("Добавлена задача: " + task);
+                }
+                else if (command == "view")
+                {
+                    Console.WriteLine("Задачи:");
+                    foreach (string todo in todos)
+                    {
+                        if (!string.IsNullOrEmpty(todo))
+                        {
+                            Console.WriteLine(todo);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестная команда.");
+                }
             }
         }
     }
