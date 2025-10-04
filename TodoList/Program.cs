42c856032f61bccac9b4e95ec0f58fc733b1f3b1
anastaflex <anastasiakabacek6@gmail.com>
2025-10-04 13:08:17 +0300

    lesson 2: creating todo list

---
+++ TodoList/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoList
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Работу выполнили Зусикова и Кабачек 3833.9");
+            Console.WriteLine("Введите ваше имя:"); 
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите вашу фамилию:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Введите ваш год рождения:");
+            int year = int.Parse(Console.ReadLine());
+            int age = DateTime.Now.Year - year;
+            
+            Console.WriteLine("Добавлен пользователь " + name + " " + surname + ", возраст - " + age);
+        }
+    }
+}
