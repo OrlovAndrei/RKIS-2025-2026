f2d9b9f84e78e312533a6a514091b730f4dfcfad
anastaflex <anastasiakabacek6@gmail.com>
2025-10-04 13:14:43 +0300

    lesson 3: add commands(help, profile, exit)

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -15,6 +15,34 @@
             int age = DateTime.Now.Year - year;
             
             Console.WriteLine("Добавлен пользователь " + name + " " + surname + ", возраст - " + age);
+            
+
+            while (true)
+            {
+                Console.WriteLine("Введите команду:");
+                string command = Console.ReadLine();
+
+                if (command == "help")
+                {
+                    Console.WriteLine("Команды:");
+                    Console.WriteLine("help — выводит список всех доступных команд с кратким описанием");
+                    Console.WriteLine("profile — выводит данные пользователя");
+                    Console.WriteLine("add \"текст задачи\" — добавляет новую задачу");
+                    Console.WriteLine("view — выводит все задачи");
+                    Console.WriteLine("exit — выход из программы");
+                }
+                else if (command == "profile")
+                {
+                    Console.WriteLine(name + " " + surname + " - " + age);
+                }
+
+                else if (command == "exit")
+                {
+                    Console.WriteLine("Выход из программы.");
+                    break;
+                }
+                
+            }
         }
     }
 }
