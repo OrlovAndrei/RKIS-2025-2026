bad7817e1abf4f786affea4b6314291d771d015f
Рублёв Иван <is.rublev1@mail.ru>
2025-10-09 23:09:28 +0300

    Добавил TodoList и README

---
+++ TodoList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите имя:");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("Введите фамилию:");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine("Введите год рождения:");
+        int birthYear = int.Parse(Console.ReadLine());
+
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthYear;
+
+        Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст - {age}");
+        Console.WriteLine("Работу выполнил Рублёв");
+    }
+}
+
+
+
+
+
