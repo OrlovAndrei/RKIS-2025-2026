47ee2564bb81e5877aaca9e834ee7d764ca62757
Angelina <degtyaryova.angilina@yandex.ru>
2025-10-17 22:15:46 +0300

    Lesson 5 Update help with descriptions of new commands and flags

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -109,8 +109,13 @@ class Program
         Console.WriteLine("help - вывести список команд");
         Console.WriteLine("profile - показать данные пользователя");
         Console.WriteLine("add - добавить задачу (формат: add \"текст задачи\")");
-        Console.WriteLine("view - показать все задачи");
-        Console.WriteLine("exit - выйти из программы");
+        Console.WriteLine("add --multiline (-m) - добавить задачу в многострочном режиме");
+        Console.WriteLine("view - показать только текст задач");
+        Console.WriteLine("view --index (-i) - показать с индексами");
+        Console.WriteLine("view --status (-s) - показать со статусами");
+        Console.WriteLine("view --update-date (-d) - показать с датами");
+        Console.WriteLine("view --all (-a) - показать всю информацию");
+        Console.WriteLine("read <номер> - просмотреть полный текст задачи");
         Console.WriteLine("done <номер> - отметить задачу выполненной");
         Console.WriteLine("delete <номер> - удалить задачу");
         Console.WriteLine("update <номер> \"текст\" - обновить текст задачи");
