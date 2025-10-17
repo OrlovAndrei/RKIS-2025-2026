11bc5838408a6c39e9d7ce8da36cca57f16c85d6
Александра Тревога <s.trevoga311@gmail.com>
2025-10-17 21:03:31 +0300

    Lesson3 code

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -21,6 +21,41 @@ namespace Todolist
             int age = DateTime.Now.Year - yearBirth;
 
             Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст – {age}");
+            Console.WriteLine("Добро пожаловать в систему управления задачами!");
+            Console.WriteLine("Введите 'help' для списка команд");
+
+            // Основной цикл программы
+            while (true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                
+                if (string.IsNullOrWhiteSpace(input))
+                    continue;
+                    
+                string[] parts = input.Split(' ');
+                string command = parts[0].ToLower();
+                
+                switch (command)
+                {
+                    case "help":
+                        ShowHelp();
+                        break;
+                    case "exit":
+                        Console.WriteLine("Выход из программы...");
+                        return;
+                    default:
+                        Console.WriteLine($"Неизвестная команда: {command}");
+                        break;
+                }
+            }
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine("Доступные команды:");
+            Console.WriteLine("help    - вывести список команд");
+            Console.WriteLine("exit    - выход из программы");
         }
     }
 }
