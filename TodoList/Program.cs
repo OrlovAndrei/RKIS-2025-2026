5b07b5c1b49a5819da14732dab9b879f2110bed0
Александра Тревога <s.trevoga311@gmail.com>
2025-10-24 18:47:14 +0300

    lesson1

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -4,10 +4,24 @@ namespace Todolist
 {
     class Program
     {
+        // Константы для "магических" значений
+        private const int InitialTodoCapacity = 2;
+        private const int ArrayGrowthFactor = 2;
+        private const int CurrentYear = 2024; // Можно заменить на DateTime.Now.Year если нужна динамичность
+
         static void Main(string[] args)
         {
             Console.WriteLine("Работу выполнили Тревога и Назаретьянц");
+            
+            UserProfile user = InitializeUserProfile();
+            TodoManager todoManager = new TodoManager(InitialTodoCapacity, ArrayGrowthFactor);
+            
+            DisplayWelcomeMessage(user);
+            RunCommandLoop(user, todoManager);
+        }
 
+        private static UserProfile InitializeUserProfile()
+        {
             Console.Write("Введите ваше имя: ");
             string firstName = Console.ReadLine();
 
@@ -15,21 +29,24 @@ namespace Todolist
             string lastName = Console.ReadLine();
 
             Console.Write("Введите ваш год рождения: ");
-            string yearBirthString = Console.ReadLine();
+            string birthYearInput = Console.ReadLine();
 
-            int yearBirth = int.Parse(yearBirthString);
-            int age = DateTime.Now.Year - yearBirth;
+            int birthYear = int.Parse(birthYearInput);
+            int age = CurrentYear - birthYear;
 
             Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст – {age}");
             
-            // Инициализация массива задач
-            string[] todos = new string[2];
-            int todoCount = 0;
-            
+            return new UserProfile(firstName, lastName, birthYear, age);
+        }
+
+        private static void DisplayWelcomeMessage(UserProfile user)
+        {
             Console.WriteLine("Добро пожаловать в систему управления задачами!");
             Console.WriteLine("Введите 'help' для списка команд");
+        }
 
-            // Основной цикл программы
+        private static void RunCommandLoop(UserProfile user, TodoManager todoManager)
+        {
             while (true)
             {
                 Console.Write("> ");
@@ -37,43 +54,121 @@ namespace Todolist
                 
                 if (string.IsNullOrWhiteSpace(input))
                     continue;
-                    
-                string[] parts = input.Split(' ');
-                string command = parts[0].ToLower();
+
+                CommandResult result = ProcessCommand(input, user, todoManager);
                 
-                switch (command)
-                {
-                    case "help":
-                        ShowHelp();
-                        break;
-                    case "profile":
-                        ShowProfile(firstName, lastName, yearBirth);
-                        break;
-                    case "add":
-                        if (parts.Length < 2)
-                        {
-                            Console.WriteLine("Ошибка: не указана задача");
-                        }
-                        else
-                        {
-                            string task = string.Join(" ", parts, 1, parts.Length - 1);
-                            AddTodo(ref todos, ref todoCount, task);
-                        }
-                        break;
-                    case "view":
-                        ViewTodos(todos, todoCount);
-                        break;
-                    case "exit":
-                        Console.WriteLine("Выход из программы...");
-                        return;
-                    default:
-                        Console.WriteLine($"Неизвестная команда: {command}");
-                        break;
-                }
+                if (result.ShouldExit)
+                    break;
             }
         }
 
-        static void ShowHelp()
+        private static CommandResult ProcessCommand(string input, UserProfile user, TodoManager todoManager)
+        {
+            string[] commandParts = input.Split(' ');
+            string command = commandParts[0].ToLower();
+            
+            switch (command)
+            {
+                case "help":
+                    CommandHandlers.ShowHelp();
+                    break;
+                case "profile":
+                    CommandHandlers.ShowProfile(user);
+                    break;
+                case "add":
+                    CommandHandlers.AddTodo(commandParts, todoManager);
+                    break;
+                case "view":
+                    CommandHandlers.ViewTodos(todoManager);
+                    break;
+                case "exit":
+                    CommandHandlers.ExitProgram();
+                    return new CommandResult(true);
+                default:
+                    Console.WriteLine($"Неизвестная команда: {command}");
+                    break;
+            }
+            
+            return new CommandResult(false);
+        }
+    }
+
+    // Класс для управления задачами
+    public class TodoManager
+    {
+        private string[] _todos;
+        private int _todoCount;
+        private readonly int _arrayGrowthFactor;
+
+        public TodoManager(int initialCapacity, int growthFactor)
+        {
+            _todos = new string[initialCapacity];
+            _todoCount = 0;
+            _arrayGrowthFactor = growthFactor;
+        }
+
+        public void AddTodo(string task)
+        {
+            if (_todoCount >= _todos.Length)
+            {
+                ResizeTodoArray();
+            }
+
+            _todos[_todoCount] = task;
+            _todoCount++;
+        }
+
+        public string[] GetTodos()
+        {
+            return _todos;
+        }
+
+        public int GetTodoCount()
+        {
+            return _todoCount;
+        }
+
+        private void ResizeTodoArray()
+        {
+            int newSize = _todos.Length * _arrayGrowthFactor;
+            string[] newTodos = new string[newSize];
+
+            for (int i = 0; i < _todos.Length; i++)
+            {
+                newTodos[i] = _todos[i];
+            }
+
+            _todos = newTodos;
+            Console.WriteLine($"Массив расширен до {_todos.Length} элементов");
+        }
+    }
+
+    // Класс для хранения профиля пользователя
+    public class UserProfile
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int BirthYear { get; }
+        public int Age { get; }
+
+        public UserProfile(string firstName, string lastName, int birthYear, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthYear = birthYear;
+            Age = age;
+        }
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+
+    // Класс для обработки команд
+    public static class CommandHandlers
+    {
+        public static void ShowHelp()
         {
             Console.WriteLine("Доступные команды:");
             Console.WriteLine("help    - вывести список команд");
@@ -83,36 +178,29 @@ namespace Todolist
             Console.WriteLine("exit    - выход из программы");
         }
 
-        static void ShowProfile(string firstName, string lastName, int birthYear)
+        public static void ShowProfile(UserProfile user)
         {
-            Console.WriteLine($"{firstName} {lastName}, {birthYear}");
+            Console.WriteLine($"{user.FirstName} {user.LastName}, {user.BirthYear}");
         }
 
-        static void AddTodo(ref string[] todos, ref int todoCount, string task)
+        public static void AddTodo(string[] commandParts, TodoManager todoManager)
         {
-            // Проверяем, нужно ли расширять массив
-            if (todoCount >= todos.Length)
+            if (commandParts.Length < 2)
             {
-                // Создаем новый массив в 2 раза больше
-                string[] newTodos = new string[todos.Length * 2];
-
-                // Копируем старые элементы
-                for (int i = 0; i < todos.Length; i++)
-                {
-                    newTodos[i] = todos[i];
-                }
-
-                todos = newTodos;
-                Console.WriteLine($"Массив расширен до {todos.Length} элементов");
+                Console.WriteLine("Ошибка: не указана задача");
+                return;
             }
 
-            // Добавляем задачу
-            todos[todoCount] = task;
-            todoCount++;
+            string task = string.Join(" ", commandParts, 1, commandParts.Length - 1);
+            todoManager.AddTodo(task);
             Console.WriteLine("Задача добавлена!");
         }
-        static void ViewTodos(string[] todos, int todoCount)
+
+        public static void ViewTodos(TodoManager todoManager)
         {
+            int todoCount = todoManager.GetTodoCount();
+            string[] todos = todoManager.GetTodos();
+
             if (todoCount == 0)
             {
                 Console.WriteLine("Список задач пуст");
@@ -125,5 +213,21 @@ namespace Todolist
                 Console.WriteLine($"{i + 1}. {todos[i]}");
             }
         }
+
+        public static void ExitProgram()
+        {
+            Console.WriteLine("Выход из программы...");
+        }
     }
-}      
+
+    // Вспомогательный класс для возврата результата выполнения команды
+    public class CommandResult
+    {
+        public bool ShouldExit { get; }
+
+        public CommandResult(bool shouldExit)
+        {
+            ShouldExit = shouldExit;
+        }
+    }
+}
