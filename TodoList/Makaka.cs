2670e4430d15f99c6bbd00df2528770c867895cd
Andrej Makaka <lvisnevskaa383@gmail.com>
2025-10-29 21:56:41 +0300

    lesson 6: add Makaka Profаил

---
+++ TodoList/Makaka.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList;
+internal class Makaka
+{
+	public string FirstName { get; private set; }
+	public string LastName { get; private set; }
+	public int BirthYear { get; private set; }
+
+	public Makaka(string firstName, string lastName, int birthYear)
+	{
+		FirstName = firstName;
+		LastName = lastName;
+		BirthYear = birthYear;
+	}
+
+	public string GetInfo()
+	{
+		int age = DateTime.Now.Year - BirthYear;
+		return $"{FirstName} {LastName}, возраст {age}";
+	}
+}
