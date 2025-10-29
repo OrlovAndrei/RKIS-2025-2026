5d496e34787ffc545b1de11c5b519d9227c3b1dd
Пётр <kekpit1997@gmail.com>
2025-10-29 20:25:10 +0300

    добавили viewcommand

---
+++ TodoList/ViewCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList1;
+
+public class ViewCommand : BaseCommand
+{
+	public bool ShowIndex { get; set; }
+	public bool ShowStatus { get; set; }
+	public bool ShowDate { get; set; }
+	public bool ShowAll { get; set; }
+
+	public override void Execute()
+	{
+		if (ShowAll)
+		{
+			todoList.View(true, true, true);
+		}
+		else
+		{
+			todoList.View(ShowIndex, ShowStatus, ShowDate);
+		}
+	}
+}
