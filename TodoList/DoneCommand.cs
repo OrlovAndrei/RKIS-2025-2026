22e8524279d75c7ce989c81696ca560d861b5de9
AV1AT <genaav1at@gmail.com>
2025-10-31 21:28:40 +0300

    Lesson 7: create done command class

---
+++ TodoList/DoneCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TodoList
+{
+    public class DoneCommand : ICommand
+    {
+        public TodoList TodoList { get; set; }
+        public int Index { get; set; }
+
+        public void Execute()
+        {
+            TodoList.GetItem(Index).MarkDone();
+            Console.WriteLine($"Задача #{Index + 1} отмечена выполненной.");
+        }
+    }
+}
