567509ea0352029b890f6b35c750893dbabbb805
AV1AT <genaav1at@gmail.com>
2025-10-31 21:52:36 +0300

    Lesson 7: Create Helpcommand class

---
+++ TodoList/HelpCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TodoList
+{
+    public class HelpCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("\nКоманды:");
+            Console.WriteLine(" add <текст> — добавить задачу");
+            Console.WriteLine(" add --multiline (-m) — добавить многострочную задачу");
+            Console.WriteLine(" done <номер> — отметить задачу выполненной");
+            Console.WriteLine(" delete <номер> — удалить задачу");
+            Console.WriteLine(" view [-i][-s][-d][-a] — показать задачи (можно комбинировать, напр. -as)");
+            Console.WriteLine(" profile — показать профиль");
+            Console.WriteLine(" help — список команд");
+            Console.WriteLine(" exit — выход");
+        }
+    }
+
+    public class ExitCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Выход из программы.");
+            Environment.Exit(0);
+        }
+    }
+}
