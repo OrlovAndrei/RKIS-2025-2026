c5f517480e371a878aa91b92733a91346adf7f0a
Рублёв Иван <is.rublev1@mail.ru>
2025-10-31 19:11:27 +0300

    lesson 7, add ProfileCommand class

---
+++ TodoList/ProfileCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList;
+
+public class ProfileCommand : ICommand
+{
+	public Profile Profile { get; set; }
+
+	public void Execute()
+	{
+		Console.WriteLine(Profile.GetInfo());
+	}
+}
