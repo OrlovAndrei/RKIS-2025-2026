dd48cdfddfbdd2389a60123d5f3cd5b5e389aadb
fein-exe <maybe.dimka@gmail.com>
2025-11-01 07:02:00 +0300

    lesson 7: add UnknownCommand

---
+++ Todolist/Todolist/UnknownCommand.cs
@@ -0,0 +1,9 @@
+namespace TodoList;
+
+public class UnknownCommand: ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Неизвестная команда. Введите 'help' для списка команд.");
+    }
+}
