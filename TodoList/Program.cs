585c96702031c91fb0bbb4349268147da5e015e8
VLAD451516 <vladpoplevin409@gmail.com>
2025-11-06 18:34:27 +0300

    lesson 2: Вывод пользователя в консоль

---
+++ TodoList/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TodoList
+{
+	internal class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Работу выполнили Поплевин и Музыка 3831");
+			Console.Write("Введите ваше имя: "); 
+			string firstName = Console.ReadLine();
+			Console.Write("Введите вашу фамилию: ");
+			string lastName = Console.ReadLine();
+
+			Console.Write("Введите ваш год рождения: ");
+			int year = int.Parse(Console.ReadLine());
+
+			Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст - {DateTime.Now.Year - year}");
+		}
+	}
+}
