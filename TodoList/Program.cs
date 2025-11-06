da3e2fd2c82c920220fab8d41ecbf06fdb8fcd1b
VLAD451516 <vladpoplevin409@gmail.com>
2025-11-06 19:04:48 +0300

    lesson 3: Добавил команды help и profile

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -14,6 +14,28 @@
 			int year = int.Parse(Console.ReadLine());
 
 			Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст - {DateTime.Now.Year - year}");
+            
+			while (true)
+			{
+				Console.Write("Введите команду: ");
+				string command = Console.ReadLine();
+
+				if (command == "help")
+				{
+					Console.WriteLine("""
+					Доступные команды:
+					help — список команд
+					profile — выводит данные профиля
+					add "текст задачи" — добавляет задачу
+					view — просмотр всех задач
+					exit — завершить программу
+					""");
+				}
+				else if (command == "profile")
+				{
+					Console.WriteLine($"{firstName} {lastName}, {year}");
+				}
+			}
 		}
 	}
 }
