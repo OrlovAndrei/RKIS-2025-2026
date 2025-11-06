a13cbb21f66e250972e1d1d12ec45627816e94b6
VLAD451516 <vladpoplevin409@gmail.com>
2025-11-06 19:15:17 +0300

    lesson 3: Добавил команды add и view

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -15,36 +15,60 @@
 
 			Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст - {DateTime.Now.Year - year}");
             
-			while (true)
-			{
-				Console.Write("Введите команду: ");
-				string command = Console.ReadLine();
+            string[] todos = new string[2];
+            int index = 0;
 
-				if (command == "help")
-				{
-					Console.WriteLine("""
-					Доступные команды:
-					help — список команд
-					profile — выводит данные профиля
-					add "текст задачи" — добавляет задачу
-					view — просмотр всех задач
-					exit — завершить программу
-					""");
-				}
-				else if (command == "profile")
-				{
-					Console.WriteLine($"{firstName} {lastName}, {year}");
-				}
-				else if (command == "exit")
-				{
-					Console.WriteLine("Программа завершена.");
-					break;
-				}
-				else
-				{
-					Console.WriteLine("Неизвестная команда. Введите help для списка команд.");
-				}
-			}
-		}
-	}
+            while (true)
+            {
+                Console.Write("Введите команду: ");
+                string command = Console.ReadLine();
+
+                if (command == "help")
+                {
+                    Console.WriteLine("""
+                    Доступные команды:
+                    help — список команд
+                    profile — выводит данные профиля
+                    add "текст задачи" — добавляет задачу
+                    view — просмотр всех задач
+                    exit — завершить программу
+                    """);
+                }
+                else if (command == "profile")
+                {
+                    Console.WriteLine($"{firstName} {lastName}, {year}");
+                }
+                else if (command.StartsWith("add "))
+                {
+                    string task = command.Split(" ", 2)[1];
+                    if (index == todos.Length)
+                    {
+                        string[] newTodos = new string[todos.Length * 2];
+                        for (int i = 0; i < todos.Length; i++)
+                        {
+                            newTodos[i] = todos[i];
+                        }
+                        todos = newTodos;
+                    }
+
+                    todos[index] = task;
+                    index++;
+                    Console.WriteLine($"Задача добавлена: {task}");
+                }
+                else if (command == "view")
+                {
+                    Console.WriteLine($"Список задач:\n{string.Join("\n", todos.Where(s => !string.IsNullOrWhiteSpace(s)))}");
+                }
+                else if (command == "exit")
+                {
+                    Console.WriteLine("Программа завершена.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестная команда. Введите help для списка команд.");
+                }
+            }
+        }
+    }
 }
