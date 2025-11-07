1bd3914bbb6932fa6d2c12b6fbabce63471a5c53
Георгий Шелепов <shelepov15945@gmail.com>
2025-11-07 21:01:42 +0300

    lesson 7: delete issues

---
+++ TodoList/Commands/AddCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace TodoList
+{
+    public class AddCommand : ICommand
+    {
+        public string Text { get; set; }
+        public bool IsMultiline { get; set; }
+        public string[] Flags { get; set; }
+        public TodoList TodoList { get; set; }
+
+        public void Execute()
+        {
+            if (TodoList == null)
+            {
+                Console.WriteLine("Ошибка: список задач не инициализирован.");
+                return;
+            }
+
+            TodoList.AddTask(Text, Flags ?? Array.Empty<string>());
+        }
+    }
+}
---
+++ TodoList/Commands/DeleteCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace TodoList
+{
+    public class DeleteCommand : ICommand
+    {
+        public string Arg { get; set; }
+        public TodoList TodoList { get; set; }
+
+        public void Execute()
+        {
+            if (TodoList == null)
+            {
+                Console.WriteLine("Ошибка: список задач не инициализирован.");
+                return;
+            }
+
+            TodoList.DeleteTask(Arg);
+        }
+    }
+}
