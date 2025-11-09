5e716ab71b88b458b3a431eca7a05c1f07ae13a3
GambItic <roman0-0@list.ru>
2025-11-09 21:39:39 +0300

    profileFix

--- TodoList/Profile.cs
+++ TodoList/Profile.cs
@@ -4,11 +4,12 @@ public class Profile
     public string FirstName;
     public string LastName;
     public int BirthYear;
+    public int Age => DateTime.Now.Year - BirthYear;
     public Profile(string firstName, string lastName, int birthYear)
     {
         FirstName = firstName;
         LastName = lastName;
         BirthYear = birthYear;
     }
-    public string GetInfo() => $"{FirstName}, {LastName}, {BirthYear}";
+    public string GetInfo() => ($"\nДобавлен пользователь: Имя: {FirstName}, Фамилия: {LastName}, Возраст: {Age}");
 }
