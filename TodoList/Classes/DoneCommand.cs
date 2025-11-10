be71ef41bf03634bd0527e572643881da66673fa
VLAD451516 <vladpoplevin409@gmail.com>
2025-11-10 18:25:35 +0300

    lesson 7: Добавление HelpCommand

--- TodoList/Classes/DoneCommand.cs
+++ TodoList/Classes/DoneCommand.cs
@@ -1,6 +1,12 @@
 namespace TodoList.Classes;
 
-public class DoneCommand
+public class DoneCommand : ICommand
 {
-	
+	public int TaskIndex { get; set; }
+	public TodoList TodoList { get; set; }
+
+	public void Execute()
+	{
+		TodoList.MarkDone(TaskIndex);
+	}
 }
