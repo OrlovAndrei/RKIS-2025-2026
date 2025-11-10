e545ee2b8ec8b74e427cc33b67f4524da905e0b6
VLAD451516 <vladpoplevin409@gmail.com>
2025-11-10 18:31:55 +0300

    lesson 7: Добавление ProfileCommand

---
+++ TodoList/Classes/ProfileCommand.cs
@@ -0,0 +1,11 @@
+namespace TodoList.Classes;
+
+public class ProfileCommand : ICommand
+{
+	public Profile Profile { get; set; }
+
+	public void Execute()
+	{
+		Console.WriteLine(Profile.GetInfo());
+	}
+}
