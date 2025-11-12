ad85ac80df3e19ec6d27c3ad937e63f49a772856
VLAD451516 <vladpoplevin409@gmail.com>
2025-11-12 19:45:32 +0300

    lesson 8: Добавил загрузку задач из csv

--- TodoList/CommandParser.cs
+++ TodoList/CommandParser.cs
@@ -5,7 +5,7 @@ namespace TodoList;
 public class CommandParser
 {
 	public static Profile profile = FileManager.LoadProfile();
-	public static TodoList todoList = new();
+	public static TodoList todoList = FileManager.LoadTodos();
 	public static ICommand Parse(string input)
 	{
 		var parts = input.Trim().Split(' ', 2);
