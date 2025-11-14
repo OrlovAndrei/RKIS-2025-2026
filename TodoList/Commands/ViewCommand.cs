28c5a7d47012bbf856e58825963cc89e4bc8e466
Георгий Шелепов <shelepov15945@gmail.com>
2025-11-14 21:22:06 +0300

    lesson 9: update ViewCommand

--- TodoList/Commands/ViewCommand.cs
+++ TodoList/Commands/ViewCommand.cs
@@ -1,30 +1,13 @@
 ﻿namespace TodoList.Commands
 {
-    public class ViewCommand : ICommand
-    {
-        public bool ShowAll { get; set; } = false;
-        public bool ShowIndex { get; set; } = false;
-        public bool ShowStatus { get; set; } = false;
-        public bool ShowDate { get; set; } = false;
-        public string[] Flags { get; set; } = Array.Empty<string>();
-        public TodoList TodoList { get; set; } = null!;
+	public class ViewCommand : ICommand
+	{
+		public string[] Flags { get; set; } = Array.Empty<string>();
+		public TodoList TodoList { get; set; }
 
-        public void Execute()
-        {
-            var flagList = new List<string>();
-
-            if (ShowAll) flagList.Add("all");
-            if (ShowIndex) flagList.Add("index");
-            if (ShowStatus) flagList.Add("status");
-            if (ShowDate) flagList.Add("update-date");
-
-            if (Flags != null)
-            {
-                flagList.AddRange(Flags);
-            }
-
-            TodoList.ViewTasks(flagList.ToArray());
-
-        }
-    }
+		public void Execute()
+		{
+			TodoList.ViewTasks(Flags);
+		}
+	}
 }
