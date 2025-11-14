e0c3c68dbebcd2196b90ba99def27e1dd39b03f7
Георгий Шелепов <shelepov15945@gmail.com>
2025-11-14 21:25:09 +0300

    lesson 9: update DoneCommand

--- TodoList/Commands/DoneCommand.cs
+++ TodoList/Commands/DoneCommand.cs
@@ -1,20 +1,34 @@
 ﻿namespace TodoList.Commands
 {
-    public class DoneCommand : ICommand
-    {
-        public string Arg { get; set; }
-        public TodoList TodoList { get; set; }
+	public class DoneCommand : ICommand
+	{
+		public string Arg { get; set; }
+		public TodoList TodoList { get; set; }
 
-        public void Execute()
-        {
-            if (TodoList == null)
-            {
-                Console.WriteLine("Ошибка: список задач не инициализирован.");
-                return;
-            }
+		public void Execute()
+		{
+			if (TodoList == null)
+			{
+				Console.WriteLine("Ошибка: список задач не инициализирован.");
+				return;
+			}
 
-            TodoList.MarkTaskDone(Arg);
-            FileManager.SaveTodos(TodoList, "data/todo.csv");
-        }
-    }
+			if (!int.TryParse(Arg, out int idx))
+			{
+				Console.WriteLine("Ошибка: укажите номер задачи");
+				return;
+			}
+
+			idx--;
+			if (idx < 0 || idx >= TodoList.tasks.Count)
+			{
+				Console.WriteLine("Ошибка: некорректный номер задачи");
+				return;
+			}
+
+			TodoList.tasks[idx].SetStatus(TodoStatus.Completed);
+			Console.WriteLine("Задача выполнена");
+			FileManager.SaveTodos(TodoList, "data/todo.csv");
+		}
+	}
 }
