325d0a177474f80f1705eb94feaa917d60ee6906
IvanMOrozov-wt <hamez4158@gmail.com>
2025-11-17 18:30:23 +0300

    lesson 2: Program

---
+++ TodoList/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Работу выполнил: Морозов Иван 3833.9");
+Console.WriteLine("Введите свое имя");
+string name = Console.ReadLine();
+Console.WriteLine("Ведите свою фамилию");
+string surname = Console.ReadLine();
+Console.WriteLine("Ведите свой год рождения");
+int date = int.Parse(Console.ReadLine());
+int age = 2025 - date;
+Console.WriteLine("Добавлен пользователь " + name + " " + surname + ", Возраст " + age);
+
