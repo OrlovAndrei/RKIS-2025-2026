22bed47f297f6582eecad169c7dacd1b0e457647
IvanMOrozov-wt <hamez4158@gmail.com>
2025-11-17 19:25:46 +0300

    lesson 4: addUser and profile

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,62 +1,74 @@
 internal class Program
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Работу выполнил: Морозов Иван 3833.9");
-        Console.WriteLine("Введите свое имя");
-        string name = Console.ReadLine();
-        Console.WriteLine("Ведите свою фамилию");
-        string surname = Console.ReadLine();
-        Console.WriteLine("Ведите свой год рождения");
-        int date = int.Parse(Console.ReadLine());
-        int age = 2025 - date;
-        Console.WriteLine("Добавлен пользователь " + name + " " + surname + ", Возраст " + age);
-        
-        int arrayLength = 2;
-        string[] todos = new string[arrayLength];
-        int currentTaskNumber = 0;
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Работу выполнил: Морозов Иван 3833.9");
 
-        while (true)
-        {
-            Console.WriteLine("Введите команду: ");
-            string userCommand = Console.ReadLine();
-            if (userCommand == "exit") break;
-            switch (userCommand.Split()[0])
-            {
-                case "help":
-	                Console.WriteLine("help - выводит список всех доступных команд\n" +
-	                                  "profile - выводит ваши данные\n" +
-	                                  "add - добавляет новую задачу\n" +
-	                                  "view - просмотр задач\n" +
-	                                  "exit - выйти");
-                    break;
-                case "profile":
-                    Console.WriteLine("Пользователь: " + name + " " + surname + ", Возраст " + age);
-                    break;
-                case "add":
-	                if (currentTaskNumber == todos.Length)
-	                {
-		                arrayLength *= 2;
-		                string[] tempTodos = new string[arrayLength];
-		                for (int i = 0; i < todos.Length; i++)
-			                tempTodos[i] = todos[i];
-		                todos = tempTodos;
-	                }
-	                string[] taskText = userCommand.Split('\"', 3);
-	                todos[currentTaskNumber] = taskText[1];
-	                currentTaskNumber++;
-                    break;
-                case "view":
-                    for (int i = 0;i < arrayLength; i++)
-                    {
-                        if (!string.IsNullOrEmpty(todos[i]))
-                            Console.WriteLine(todos[i]);
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Неправильно введена команда");
-                    break;
-            }
-        }
-    }
+		string name, surname;
+		int age;
+		AddUser(out name, out surname, out age);
+
+		var arrayLength = 2;
+		var todos = new string[arrayLength];
+		var currentTaskNumber = 0;
+
+		while (true)
+		{
+			Console.WriteLine("Введите команду: ");
+			var userCommand = Console.ReadLine();
+			if (userCommand == "exit") break;
+			switch (userCommand.Split()[0])
+			{
+				case "help":
+					Console.WriteLine("help - выводит список всех доступных команд\n" +
+					                  "profile - выводит ваши данные\n" +
+					                  "add - добавляет новую задачу\n" +
+					                  "view - просмотр задач\n" +
+					                  "exit - выйти");
+					break;
+				case "profile":
+					GetUserInfo(name, surname, age);
+					break;
+				case "add":
+					if (currentTaskNumber == todos.Length)
+					{
+						arrayLength *= 2;
+						var tempTodos = new string[arrayLength];
+						for (var i = 0; i < todos.Length; i++)
+							tempTodos[i] = todos[i];
+						todos = tempTodos;
+					}
+
+					var taskText = userCommand.Split('\"', 3);
+					todos[currentTaskNumber] = taskText[1];
+					currentTaskNumber++;
+					break;
+				case "view":
+					for (var i = 0; i < arrayLength; i++)
+						if (!string.IsNullOrEmpty(todos[i]))
+							Console.WriteLine(todos[i]);
+					break;
+				default:
+					Console.WriteLine("Неправильно введена команда");
+					break;
+			}
+		}
+	}
+
+	private static void AddUser(out string name, out string surname, out int age)
+	{
+		Console.WriteLine("Введите свое имя");
+		name = Console.ReadLine();
+		Console.WriteLine("Ведите свою фамилию");
+		surname = Console.ReadLine();
+		Console.WriteLine("Ведите свой год рождения");
+		int date = int.Parse(Console.ReadLine());
+		age = 2025 - date;
+		Console.WriteLine("Добавлен пользователь " + name + " " + surname + ", Возраст " + age);
+	}
+
+	private static void GetUserInfo(string name, string surname, int age)
+	{
+		Console.WriteLine("Пользователь: " + name + " " + surname + ", возраст: " + age);
+	}
 }
