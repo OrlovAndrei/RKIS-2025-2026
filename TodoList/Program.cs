1bd78c1fa68dfe399e9a079b59d9e7fe049d31ef
Danil8467 <danil74564@mail.ru>
2025-11-18 19:21:22 +0300

    lesson 2: Creating program

---
+++ TodoList/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TodoList
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Работу  выполнили Лютов и Легатов 3832");
+			Console.Write("Введите ваше имя: "); 
+			string firstName = Console.ReadLine();
+			Console.Write("Введите вашу фамилию: ");
+			string lastName = Console.ReadLine();
+
+			Console.Write("Введите ваш год рождения: ");
+			int year = int.Parse(Console.ReadLine());
+			int age = DateTime.Now.Year - year;
+			
+			string text = "Добавлен пользователь " + firstName + " " + lastName + ", возраст - " + age;
+			Console.WriteLine(text);
+		}
+	}
+}
