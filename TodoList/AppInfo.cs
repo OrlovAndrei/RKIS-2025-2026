7cf0551d8e1bc37e2393ee6b192fa42c89af4fa4
Angelina <degtyaryova.angilina@yandex.ru>
2025-11-21 17:01:55 +0300

    Lesson 10 Creating a static AppInfo class with undo/redo stacks

---
+++ TodoList/AppInfo.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+public static class AppInfo
+{
+    public static TodoList Todos { get; set; }
+    public static Profile CurrentProfile { get; set; }
+    public static Stack<ICommand> UndoStack { get; set; }
+    public static Stack<ICommand> RedoStack { get; set; }
+
+    static AppInfo()
+    {
+        Todos = new TodoList();
+        UndoStack = new Stack<ICommand>();
+        RedoStack = new Stack<ICommand>();
+    }
+}
