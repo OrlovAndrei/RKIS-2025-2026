efa4f8b9ab4a12d6a8038080d2bb1f9852a52df5
exti4 <oneh3248@gmail.com>
2025-11-24 20:21:17 +0300

    lesson 2: setup Program

---
+++ TodoList/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TodoList;
+
+internal class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Работу выполнили Антонов и Мадойкин 3833");
+		Console.Write("Введите имя: ");
+		var name = Console.ReadLine();
+		Console.Write("Введите фамилию: ");
+		var surname = Console.ReadLine();
+
+		Console.Write("Введите год рождения: ");
+		var year = int.Parse(Console.ReadLine());
+		var age = DateTime.Now.Year - year;
+
+		Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
+	}
+}
