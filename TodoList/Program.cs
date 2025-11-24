6e8a68162a5ada3afe1b2d436dfdfbf721e4b087
exti4 <oneh3248@gmail.com>
2025-11-24 20:37:24 +0300

    lesson 3: add profile and help

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -15,5 +15,27 @@ internal class Program
 		var age = DateTime.Now.Year - year;
 
 		Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
+
+		while (true)
+		{
+			Console.Write("\nВведите команду: ");
+			var command = Console.ReadLine();
+
+			if (command == "help") Console.WriteLine(
+					"""
+					Доступные команды:
+					help — список команд
+					profile — выводит данные профиля
+					exit — завершить программу
+					"""
+				);
+			else if (command == "profile") Console.WriteLine($"{name} {surname}, {year}");
+			else if (command == "exit")
+			{
+				Console.WriteLine("Программа завершена.");
+				break;
+			}
+			else Console.WriteLine("Неизвестная команда. Введите help для списка команд.");
+		}
 	}
 }
