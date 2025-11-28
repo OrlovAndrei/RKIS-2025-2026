00b4bf690c6cbdbd2a9224e7a8b33bd48ab1655d
exti4 <oneh3248@gmail.com>
2025-11-28 20:12:32 +0300

    lesson 6: TodoItem class

---
+++ TodoList/TodoItem.cs
@@ -0,0 +1,43 @@
+namespace TodoList;
+
+public class TodoItem
+{
+	public TodoItem(string text)
+	{
+		Text = text;
+		IsDone = false;
+		LastUpdate = DateTime.Now;
+	}
+
+	public string Text { get; private set; }
+	public bool IsDone { get; private set; }
+	public DateTime LastUpdate { get; private set; }
+
+	public void MarkDone()
+	{
+		IsDone = true;
+		LastUpdate = DateTime.Now;
+	}
+
+	public void UpdateText(string newText)
+	{
+		Text = newText;
+		LastUpdate = DateTime.Now;
+	}
+
+	public string GetShortInfo()
+	{
+		var text = Text.Replace("\r", " ").Replace("\n", " ");
+		if (text.Length > 30) text = text.Substring(0, 30) + "...";
+
+		return text;
+	}
+
+	public string GetFullInfo(int index)
+	{
+		return
+			$"Текст: {Text}" +
+			$"Статус: {(IsDone ? "Выполнено" : "Не выполнено")}" +
+			$"Изменено: {LastUpdate:dd.MM.yyyy HH:mm:ss}";
+	}
+}
