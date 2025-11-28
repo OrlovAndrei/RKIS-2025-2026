41cf4f8793e450804b4168640f39f2153782d754
Aleks55466 <aleksejbagdanovic761@gmail.com>
2025-11-28 21:01:22 +0300

    Lesson 10: Создали AppInfo

---
+++ TodoList/AppInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Todolist
+{
+    public static class AppInfo
+    {
+        public static Todolist Todos { get; set; } = new Todolist();
+        public static Profile CurrentProfile { get; set; }
+        public static Stack<ICommand> UndoStack { get; set; } = new Stack<ICommand>();
+        public static Stack<ICommand> RedoStack { get; set; } = new Stack<ICommand>();
+    }
+}
