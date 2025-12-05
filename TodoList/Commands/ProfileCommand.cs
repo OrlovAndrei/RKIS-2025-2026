a43e82f798dde15a6f686672cde9df9f76bc9bfa
Артем Бешкеев <11071980ir@gmail.com>
2025-12-06 00:31:10 +0300

    Lesson 11: модифицировали команды profile

--- TodoList/Commands/ProfileCommand.cs
+++ TodoList/Commands/ProfileCommand.cs
@@ -2,9 +2,16 @@ namespace TodoList
 {
     public class ProfileCommand : ICommand
     {
+        private readonly Profile _profile;
+
+        public ProfileCommand(Profile profile)
+        {
+            _profile = profile;
+        }
+
         public void Execute()
         {
-            Console.WriteLine(AppInfo.CurrentProfile.GetInfo());
+            Console.WriteLine(_profile.GetInfo());
         }
 
         public void Unexecute() { }
--- TodoList/Profile.cs
+++ TodoList/Profile.cs
@@ -1,16 +1,30 @@
-using System;
-
 namespace TodoList
 {
     public class Profile
     {
+        public Guid Id { get; private set; }
+        public string Login { get; private set; }
+        public string Password { get; private set; }
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public int BirthYear { get; private set; }
         public int Age => DateTime.Now.Year - BirthYear;
 
+        public Profile(Guid id, string login, string password, string firstName, string lastName, int birthYear)
+        {
+            Id = id;
+            Login = login;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthYear = birthYear;
+        }
+
         public Profile(string firstName, string lastName, int birthYear)
         {
+            Id = Guid.NewGuid();
+            Login = "user"; 
+            Password = "password"; 
             FirstName = firstName;
             LastName = lastName;
             BirthYear = birthYear;
@@ -20,8 +34,10 @@ namespace TodoList
 
         public string GetInfo()
         {
-            return $"{FirstName} {LastName}, возраст {Age}";
+            return $"{FirstName} {LastName}, возраст {Age} (логин: {Login})";
         }
+
+        public bool CheckPassword(string password) => Password == password;
 
         public void Update(string firstName, string lastName, int birthYear)
         {
