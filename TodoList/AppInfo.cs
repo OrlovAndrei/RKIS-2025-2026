6b7edb93f93771175274a3cba261f8a212c6b97f
Angelina <degtyaryova.angilina@yandex.ru>
2025-12-05 20:42:15 +0300

    Lesson 11 Updating AppInfo to store multiple profiles and the UserTodos dictionary

--- TodoList/AppInfo.cs
+++ TodoList/AppInfo.cs
@@ -1,16 +1,38 @@
 using System.Collections.Generic;
+using System;
 
 public static class AppInfo
 {
-    public static TodoList Todos { get; set; }
-    public static Profile CurrentProfile { get; set; }
+    public static Dictionary<Guid, TodoList> UserTodos { get; set; }
+    public static List<Profile> Profiles { get; set; }
+    public static Guid? CurrentProfileId { get; set; }
     public static Stack<ICommand> UndoStack { get; set; }
     public static Stack<ICommand> RedoStack { get; set; }
 
     static AppInfo()
     {
-        Todos = new TodoList();
+        UserTodos = new Dictionary<Guid, TodoList>();
+        Profiles = new List<Profile>();
         UndoStack = new Stack<ICommand>();
         RedoStack = new Stack<ICommand>();
+    }
+    public static TodoList CurrentTodoList
+    {
+        get
+        {
+            if (CurrentProfileId.HasValue && UserTodos.ContainsKey(CurrentProfileId.Value))
+                return UserTodos[CurrentProfileId.Value];
+            return null;
+        }
+    }
+
+    public static Profile CurrentProfile
+    {
+        get
+        {
+            if (CurrentProfileId.HasValue)
+                return Profiles.Find(p => p.Id == CurrentProfileId.Value);
+            return null;
+        }
     }
 }
