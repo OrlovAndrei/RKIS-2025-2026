ce929daac02bb631b5dce3e7e55fab70b1d2fc8d
Angelina <degtyaryova.angilina@yandex.ru>
2025-12-05 20:35:58 +0300

    Lesson 11 Modifying the Profile class by adding Id, Login, and Password

--- TodoList/Profile.cs
+++ TodoList/Profile.cs
@@ -2,12 +2,18 @@ using System;
 
 public class Profile
 {
+    public Guid Id { get; private set; }
+    public string Login { get; private set; }
+    public string Password { get; private set; }
     public string FirstName { get; private set; }
     public string LastName { get; private set; }
     public int BirthYear { get; private set; }
 
-    public Profile(string firstName, string lastName, int birthYear)
+    public Profile(Guid id, string login, string password, string firstName, string lastName, int birthYear)
     {
+        Id = id;
+        Login = login ?? "user";
+        Password = password ?? "";
         FirstName = firstName;
         LastName = lastName;
         BirthYear = birthYear;
@@ -16,6 +22,10 @@ public class Profile
     public string GetInfo()
     {
         int age = DateTime.Now.Year - BirthYear;
-        return $"{FirstName} {LastName}, возраст {age}";
+        return $"{FirstName} {LastName}, возраст {age}, логин: { Login}";
+    }
+    public bool CheckPassword(string password)
+    {
+        return Password == password;
     }
 }
