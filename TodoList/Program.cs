944ef59cff944bd2e6536b290ea76bfd01f0d52c
tuchaxxx <timofeiturchin@gmail.com>
2025-12-08 18:27:28 +0300

    lesson 2: создание программы

---
+++ TodoList/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoList
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Работу выполнил Турчин 3833.9");
+			Console.Write("Введите имя: "); 
+			string name = Console.ReadLine();
+			Console.Write("Введите фамилию: ");
+			string surname = Console.ReadLine();
+
+			Console.Write("Введите год рождения: ");
+			int year = int.Parse(Console.ReadLine());
+			int age = DateTime.Now.Year - year;
+
+			Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
+		}
+	}
+}
