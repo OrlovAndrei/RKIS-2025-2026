960d2b75a38e4a5b3a2472a771a7510ec8ab8f42
Sdq5ekFly <Sdq_5ekFly@mail.ru>
2025-12-08 18:36:14 +0300

    lesson 3: add and view commands

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -17,7 +17,9 @@
             
 			Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
 			
+			string[] todos = new string[2];
 			bool isRunning = true;
+			int taskCount = 0;
 
 			while (isRunning)
 			{
@@ -29,12 +31,48 @@
 					Console.WriteLine("""
 					Доступные команды:
 					help — список команд
-					profile — выводит данные профиля
+					profile — выводит данные 
+					add "текст" — добавляет задачу
+					view — просмотр всех задач
+					exit — завершить программу
 					""");
 				}
 				else if (command == "profile")
 				{
 					Console.WriteLine($"{name} {surname} {age}");
+				}
+				else if (command.StartsWith("add "))
+				{
+					string task = command.Split(" ", 2)[1];
+					if (taskCount == todos.Length)
+					{
+						string[] newTodos = new string[todos.Length * 2];
+						for (int i = 0; i < todos.Length; i++)
+						{
+							newTodos[i] = todos[i];
+						}
+						todos = newTodos;
+					}
+
+					todos[taskCount] = task;
+					taskCount++;
+					Console.WriteLine($"Задача добавлена: {task}");
+				}
+				else if (command == "view")
+				{
+					Console.WriteLine("Задачи:");
+					foreach (string todo in todos)
+					{
+						if (!string.IsNullOrEmpty(todo))
+						{
+							Console.WriteLine(todo);
+						}
+					}
+				}
+				else if (command == "exit")
+				{
+					Console.WriteLine("Программа завершена.");
+					isRunning = false;
 				}
 				else
 				{
