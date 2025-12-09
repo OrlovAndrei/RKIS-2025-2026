45e0248c14180ebf6603f7301dd9c4cdf7264836
Ренат Измайлов <renatizmaylov07@mail.ru>
2025-12-10 02:30:26 +0300

    lesson 3: adding commands view and add

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -2,20 +2,23 @@
 {
     class Program
     {
-        public static void Main()
-        {
-            Console.WriteLine("Работу выполнил: Измайлов");
+		public static void Main()
+		{
+			Console.WriteLine("Работу выполнил: Измайлов");
 
-            Console.Write("Введите ваше имя: ");
-            string name = Console.ReadLine();
-            Console.Write("Введите вашу фамилию: ");
-            string surname = Console.ReadLine();
+			Console.Write("Введите ваше имя: ");
+			string name = Console.ReadLine();
+			Console.Write("Введите вашу фамилию: ");
+			string surname = Console.ReadLine();
 
-            Console.Write("Введите ваш год рождения: ");
-            int year = int.Parse(Console.ReadLine());
-            int age = DateTime.Now.Year - year;
+			Console.Write("Введите ваш год рождения: ");
+			int year = int.Parse(Console.ReadLine());
+			int age = DateTime.Now.Year - year;
 
-            Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
+			Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
+		}
+			string[] todos = new string[2];
+			int index = 0;
 			while (true)
 			{
 				Console.Write("Введите команду: ");
@@ -35,6 +38,30 @@
 				else if (command == "profile")
 				{
 					Console.WriteLine($"{firstName} {lastName}, {year}");
+				}
+				else if (input.StartsWith("add"))
+				{
+					string task = input.Split(" ", 2)[1];
+					if (index >= todos.Length)
+					{
+						string[] newTodos = new string[todos.Length * 2];
+						for (int i = 0; i < todos.Length; i++)
+							newTodos[i] = todos[i];
+
+						todos = newTodos;
+					}
+
+					todos[index] = task;
+					index++;
+					Console.WriteLine($"Задача добавлена: {task}");
+				}
+				else if (input == "view")
+				{
+					Console.WriteLine("Список задач:");
+					for (int i = 0; i < index; i++)
+					{
+						Console.WriteLine($"{i + 1}) {todos[i]}");
+					}
 				}
 				else if (command == "exit")
 				{
