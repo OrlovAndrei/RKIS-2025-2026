f35d57970eebc26540982207743b109fc8b878dd
flairov <feelzonf@gmail.com>
2025-12-09 19:17:24 +0300

    lesson 2: создание программы todolist

---
+++ TodoList/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoList
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Работу выполнили: Десятун и Пономаренко 3833");
+            
+			Console.Write("Введите ваше имя: ");
+			string name = Console.ReadLine();
+			Console.Write("Введите вашу фамилию: ");
+			string surname = Console.ReadLine();
+
+			Console.Write("Введите ваш год рождения: ");
+			int year = int.Parse(Console.ReadLine());
+            
+			Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {DateTime.Now.Year - year}");
+		}
+	}
+}
