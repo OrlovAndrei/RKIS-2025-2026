5bbbf4983fee15b2e5a0b181eb1e7ec767ed9da6
Ренат Измайлов <renatizmaylov07@mail.ru>
2025-12-10 02:15:36 +0300

    lesson 3 : adding commands

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -16,6 +16,35 @@
             int age = DateTime.Now.Year - year;
 
             Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
-        }
+			while (true)
+			{
+				Console.Write("Введите команду: ");
+				string command = Console.ReadLine();
+
+				if (command == "help")
+				{
+					Console.WriteLine("""
+					Доступные команды:
+					help — список команд
+					profile — выводит данные профиля
+					add "текст задачи" — добавляет задачу
+					view — просмотр всех задач
+					exit — завершить программу
+					""");
+				}
+				else if (command == "profile")
+				{
+					Console.WriteLine($"{firstName} {lastName}, {year}");
+				}
+				else if (command == "exit")
+				{
+					Console.WriteLine("Программа завершена.");
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Неизвестная команда. Введите help для списка команд.");
+				}
+			}
     }
 }
