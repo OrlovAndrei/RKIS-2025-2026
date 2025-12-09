4030ac982c372caf2ddc7912af1c7176c90d3503
flairov <feelzonf@gmail.com>
2025-12-09 19:29:26 +0300

    реализовал add и view команды

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -16,6 +16,9 @@
 			var age = DateTime.Now.Year - year;
 			Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
 			
+			string[] todos = new string[2];
+			int todosCount = 0;
+
 			while (true)
 			{
 				Console.WriteLine("\nВведите команду: ");
@@ -26,10 +29,40 @@
 					Console.WriteLine("Команды:");
 					Console.WriteLine("help — выводит список всех доступных команд с кратким описанием");
 					Console.WriteLine("profile — выводит данные пользователя");
+					Console.WriteLine("exit — выход из программы");
 				}
 				else if (userInput == "profile")
 				{
 					Console.WriteLine($"{name} {surname} {age}");
+				}
+				else if (userInput.StartsWith("add "))
+				{
+					string task = userInput.Split("add ")[1];
+					if (todosCount == todos.Length)
+					{
+						string[] newTodos = new string[todos.Length * 2];
+						for (int i = 0; i < todos.Length; i++)
+						{
+							newTodos[i] = todos[i];
+						}
+
+						todos = newTodos;
+					}
+
+					todos[todosCount] = task;
+					todosCount++;
+
+					Console.WriteLine("Добавлена задача: " + task);
+				}
+				else if (userInput == "view")
+				{
+					foreach (string todo in todos)
+					{
+						if (!string.IsNullOrEmpty(todo))
+						{
+							Console.WriteLine(todo);
+						}
+					}
 				}
 				else
 				{
