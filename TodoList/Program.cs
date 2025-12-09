c8aecf526fe137300db46d67d62f5e276bc47889
nikitafilimonov580 <nikitafilimonov580@gmail.com>
2025-12-09 19:28:47 +0300

    lesson 2: создание программы списка дел

---
+++ TodoList/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Работу выполнили: Филимонов и Фаградян 3833.9");
+		Console.WriteLine("Введите свое имя");
+		string name = Console.ReadLine();
+		Console.WriteLine("Ведите свою фамилию");
+		string surname = Console.ReadLine();
+		Console.WriteLine("Ведите свой год рождения");
+		int date = int.Parse(Console.ReadLine());
+		int age = 2025 - date;
+		Console.WriteLine("Добавлен пользователь " + name + " " + surname + ", Возраст " + age);
+	}
+}
