2bf41fb217ff5fe4d17929850146befdf87e0421
nikitafilimonov580 <nikitafilimonov580@gmail.com>
2025-12-10 18:49:58 +0300

    lesson 3: добавлены команды add и view, обновлён help

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -12,6 +12,9 @@
 		int age = 2025 - date;
 		Console.WriteLine("Добавлен пользователь " + name + " " + surname + ", Возраст " + age);
 		
+		int arrayLength = 2;
+		string[] todos = new string[arrayLength];
+        
 		while (true)
 		{
 			Console.WriteLine("Введите команду: для помощи напиши команду help");
@@ -20,10 +23,30 @@
 			{
 				case "help":
 					Console.WriteLine("help - выводит список всех доступных команд\n" +
-					                  "profile - выводит ваши данные");
+					                  "profile - выводит ваши данные\n" +
+					                  "add - добавляет новую задачу\n" +
+					                  "view - просмотр задач");
 					break;
 				case "profile":
 					Console.WriteLine("Пользователь: " + name + " " + surname + ", Возраст " + age);
+					break;
+				case "add":
+					for (int i = 0; i < arrayLength; i++)
+					{
+						if (string.IsNullOrEmpty(todos[i]))
+						{
+							Console.WriteLine("Напишите задачу которую необходимо добавить");
+							todos[i] = Console.ReadLine();
+							break;
+						}
+					}
+					break;
+				case "view":
+					for (int i = 0;i < arrayLength; i++)
+					{
+						if (!string.IsNullOrEmpty(todos[i]))
+							Console.WriteLine(todos[i]);
+					}
 					break;
 				default:
 					Console.WriteLine("Неправильно введена команда");
