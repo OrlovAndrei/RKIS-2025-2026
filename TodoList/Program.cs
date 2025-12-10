b3ed3dc858aeb2a0a37705fca15f98dbfba1c087
nikitafilimonov580 <nikitafilimonov580@gmail.com>
2025-12-10 18:21:39 +0300

    lesson 3: добавлен help и profile

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -11,5 +11,24 @@
 		int date = int.Parse(Console.ReadLine());
 		int age = 2025 - date;
 		Console.WriteLine("Добавлен пользователь " + name + " " + surname + ", Возраст " + age);
+		
+		while (true)
+		{
+			Console.WriteLine("Введите команду: для помощи напиши команду help");
+			string userCommand = Console.ReadLine();
+			switch (userCommand.Split()[0])
+			{
+				case "help":
+					Console.WriteLine("help - выводит список всех доступных команд\n" +
+					                  "profile - выводит ваши данные");
+					break;
+				case "profile":
+					Console.WriteLine("Пользователь: " + name + " " + surname + ", Возраст " + age);
+					break;
+				default:
+					Console.WriteLine("Неправильно введена команда");
+					break;
+			}
+		}
 	}
 }
