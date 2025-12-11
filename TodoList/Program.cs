22b6660e088676641379530f2adc4cdd14c51ead
Kot-era <gegor7085@gmail.com>
2025-12-11 20:33:46 +0300

    lesson 2: basic program

---
+++ TodoList/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoList
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Выполнил Герасимов Егор");
+			Console.Write("Введите имя: "); 
+			string firstName = Console.ReadLine();
+			Console.Write("Введите фамилию: ");
+			string lastName = Console.ReadLine();
+
+			Console.Write("Введите год рождения: ");
+			int year = int.Parse(Console.ReadLine());
+
+			string output = "Добавлен пользователь " + firstName + " " + lastName + ", возраст - " + (DateTime.Now.Year - year);
+			Console.WriteLine(output);
+		}
+	}
+}
