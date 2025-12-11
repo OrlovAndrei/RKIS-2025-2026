81b75c48f818e090c13da0dd30e6478caa54f8e4
Kot-era <gegor7085@gmail.com>
2025-12-11 20:42:13 +0300

    lesson 3: add todos and profile command

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -12,9 +12,43 @@
 
 			Console.Write("Введите год рождения: ");
 			int year = int.Parse(Console.ReadLine());
-
-			string output = "Добавлен пользователь " + firstName + " " + lastName + ", возраст - " + (DateTime.Now.Year - year);
+			int age = (DateTime.Now.Year - year);
+			
+			string output = "Добавлен пользователь " + firstName + " " + lastName + ", возраст - " + age;
 			Console.WriteLine(output);
+			
+			string[] todos = new string[2];
+			int index = 0;
+			
+			while (true)
+            {
+                Console.Write("Введите команду: ");
+                string command = Console.ReadLine();
+
+                if (command == "profile")
+                {
+                    Console.WriteLine(firstName + " " + lastName + ", - " + age);
+                }
+				else if (command.StartsWith("add "))
+                {
+                    string task = command.Split("add ")[1];
+                    if (index == todos.Length)
+                    {
+                        string[] newTodos = new string[todos.Length*2];
+                        for (int i = 0; i < todos.Length; i++)
+                        {
+                            newTodos[i] = todos[i];
+                        }
+
+                        todos = newTodos;
+                    }
+
+                    todos[index] = task;
+                    index++;
+
+                    Console.WriteLine("Добавлена задача: " + task);
+                }
+            }
 		}
 	}
 }
