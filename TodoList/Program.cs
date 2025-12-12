f1bca7b2c5f3f97d3ced2d484ea21df9b1814502
Kot-era <gegor7085@gmail.com>
2025-12-12 18:40:17 +0300

    lesson 5: update help

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -126,6 +126,9 @@
 			Console.WriteLine("help — выводит список всех доступных команд с кратким описанием");
 			Console.WriteLine("profile — выводит данные пользователя");
 			Console.WriteLine("add \"текст задачи\" — добавляет новую задачу");
+			Console.WriteLine("done idx - отмечает задачу выполненной");
+			Console.WriteLine("delete idx - удаляет задачу");
+			Console.WriteLine("update idx \"текст задачи\" - обновляет задачу");
 			Console.WriteLine("view — выводит все задачи");
 			Console.WriteLine("exit — выход из программы");
 		}
