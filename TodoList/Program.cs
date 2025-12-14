d335e15e5daf406d7ab590231404075e0e11a0c7
nikitafilimonov580 <nikitafilimonov580@gmail.com>
2025-12-14 18:02:34 +0300

    lesson 4: реализованы массивы статусов и дат

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -3,17 +3,15 @@
 	public static void Main(string[] args)
 	{
 		Console.WriteLine("Работу выполнили: Филимонов и Фаградян 3833.9");
-		Console.WriteLine("Введите свое имя");
-		string name = Console.ReadLine();
-		Console.WriteLine("Ведите свою фамилию");
-		string surname = Console.ReadLine();
-		Console.WriteLine("Ведите свой год рождения");
-		int date = int.Parse(Console.ReadLine());
-		int age = 2025 - date;
-		Console.WriteLine("Добавлен пользователь " + name + " " + surname + ", Возраст " + age);
 		
-		int arrayLength = 2;
-		string[] todos = new string[arrayLength];
+		string name, surname;
+		int age;
+		AddUser(out name, out surname, out age);
+		
+		var arrayLength = 2;
+		var todos = new string[arrayLength];
+		var statuses = new bool[arrayLength];
+		var dates = new DateTime[arrayLength];
 		int currentTaskNumber = 0;
 		
 		while (true)
@@ -34,34 +32,64 @@
 					break;
 				case "add":
 					if (currentTaskNumber == todos.Length)
-					{
-						arrayLength *= 2;
-						string[] tempTodos = new string[arrayLength];
-						for (int i = 0; i < todos.Length; i++)
-							tempTodos[i] = todos[i];
-						todos = tempTodos;
-					}
-					for (int i = 0; i < arrayLength; i++)
-					{
-						if (string.IsNullOrEmpty(todos[i]))
-						{
-							Console.WriteLine("Напишите задачу которую необходимо добавить");
-							todos[i] = Console.ReadLine();
-							break;
-						}
-					}
+						ArrayExpansion(ref todos, ref statuses, ref dates);
+					AddTask(todos, statuses, dates, ref currentTaskNumber, userCommand);
 					break;
 				case "view":
-					for (int i = 0;i < arrayLength; i++)
-					{
-						if (!string.IsNullOrEmpty(todos[i]))
-							Console.WriteLine(todos[i]);
-					}
+					ViewTasks(todos, statuses, dates);
 					break;
 				default:
 					Console.WriteLine("Неправильно введена команда");
 					break;
 			}
 		}
+	}
+	
+	private static void AddUser(out string name, out string surname, out int age)
+	{
+		Console.WriteLine("Введите свое имя");
+		name = Console.ReadLine();
+		Console.WriteLine("Ведите свою фамилию");
+		surname = Console.ReadLine();
+		Console.WriteLine("Ведите свой год рождения");
+		int date = int.Parse(Console.ReadLine());
+		age = 2025 - date;
+		Console.WriteLine("Добавлен пользователь " + GetUserInfo(name, surname, age));
+	}
+
+	private static string GetUserInfo(string name, string surname, int age) =>
+		name + " " + surname + ", возраст: " + age;
+	
+	private static void AddTask(string[] todos, bool[] statuses, DateTime[] dates, ref int currentTaskNumber, string task)
+	{
+		var taskText = task.Split('\"', 3);
+		todos[currentTaskNumber] = taskText[1];
+		dates[currentTaskNumber] = DateTime.Now;
+		statuses[currentTaskNumber] = false;
+		currentTaskNumber++;
+	}
+	private static void ViewTasks(string[] todos, bool[] statuses, DateTime[] dates)
+	{
+		for (int i = 0; i < todos.Length; i++)
+		{
+			if (!string.IsNullOrEmpty(todos[i]))
+				Console.WriteLine($"{i} {todos[i]} {statuses[i]} {dates[i]}");
+		}
+	}
+	private static void ArrayExpansion(ref string[] todos, ref bool[] statuses, ref DateTime[] dates)
+	{
+		var tempTodos = new string[todos.Length * 2];
+		var tempDates = new DateTime[todos.Length * 2];
+		var tempStatuses = new bool[todos.Length * 2];
+		for (var i = 0; i < todos.Length; i++)
+		{
+			tempTodos[i] = todos[i];
+			tempDates[i] = dates[i];
+			tempStatuses[i] = statuses[i];
+		}
+
+		todos = tempTodos;
+		dates = tempDates;
+		statuses = tempStatuses;
 	}
 }
