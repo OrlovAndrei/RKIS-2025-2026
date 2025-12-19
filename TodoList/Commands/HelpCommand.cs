a34d50614437e556233340ed4c43324645b9f104
exit4 <oneh3248@gmail.com>
2025-12-19 16:31:12 +0300

    lesson 7: add HelpCommand

---
+++ TodoList/Commands/HelpCommand.cs
@@ -0,0 +1,21 @@
+namespace TodoList.Commands;
+
+public class HelpCommand : ICommand
+{
+	public void Execute()
+	{
+		Console.WriteLine("""
+		                  Доступные команды:
+		                  help — список команд
+		                  profile — выводит данные профиля
+		                  add "текст задачи" — добавляет задачу
+		                    Флаги: --multiline -m
+		                  done - отметить выполненным
+		                  delete - удалить задачу
+		                  view — просмотр всех задач
+		                    Флаги: --index -i, --status -s, --update-date -d, --all -a
+		                  exit — завершить программу
+		                  read - посмотреть полный текст задачи
+		                  """);
+	}
+}
