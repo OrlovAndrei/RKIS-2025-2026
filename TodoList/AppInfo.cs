5ed18a049ad4e4404c626f7a7702955803686b0e
Георгий Шелепов <shelepov15945@gmail.com>
2026-03-07 21:06:47 +0300

    lesson15: add Storage property to AppInfo

--- TodoList/AppInfo.cs
+++ TodoList/AppInfo.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using TodoList.Commands;
+using TodoList.Interfaces;
 
 namespace TodoList
 {
 	public static class AppInfo
 	{
+		public static IDataStorage Storage { get; set; } = null!;
+		
 		public static Dictionary<Guid, TodoList> AllTodos { get; set; } = new Dictionary<Guid, TodoList>();
 
 		public static List<Profile> AllProfiles { get; set; } = new List<Profile>();
