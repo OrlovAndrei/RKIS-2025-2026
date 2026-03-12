073df78f96486f6fd27fcef9574742e78d77a3bf
Kotyaraa <mckrakazyabra@gmail.com>
2026-03-12 20:08:21 +0300

    Sm 2 Lesson 4:  EncryptionSettings

---
+++ TodoList/TodoList/Commands/EncryptionSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+public static class EncryptionSettings
+{
+	public static readonly byte[] Key = Encoding.UTF8.GetBytes("ThisIsASecretKey12345");
+	public static readonly byte[] IV = Encoding.UTF8.GetBytes("ThisIsAnIv123456");
+	static EncryptionSettings()
+	{
+		if (Key.Length != 16 && Key.Length != 24 && Key.Length != 32)
+		{
+			throw new CryptographicException("Encryption key must be 16, 24, or 32 bytes long.");
+		}
+		if (IV.Length != 16)
+		{
+			throw new CryptographicException("Encryption IV must be 16 bytes long.");
+		}
+	}
+}
