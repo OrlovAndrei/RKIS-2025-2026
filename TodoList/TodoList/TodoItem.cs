e63554e5f6b7604ea42287cf05858d79f429e996
Arina Kabanova <rinistorbrows3r@gmail.com>
2026-04-09 16:24:37 +0300

    Sm 2 Lesson 3: TodoItem NEW

--- TodoList/TodoList/TodoItem.cs
+++ TodoList/TodoList/TodoItem.cs
@@ -1,5 +1,6 @@
-﻿using TodoApp.Exceptions;
-public enum TodoStatus
+﻿using TodoApp;
+using TodoApp.Exceptions;
+	public enum TodoStatus
 {
 	NotStarted,    // не начато
 	InProgress,    // в процессе
@@ -7,54 +8,70 @@ public enum TodoStatus
 	Postponed,     // отложено
 	Failed         // провалено
 }
+
 public class TodoItem
 {
+	private readonly IClock _clock;
 	private string _text;
 	private TodoStatus _status;
 	private DateTime _lastUpdate;
-	public TodoItem(string text)
+
+	// Конструктор с IClock (по умолчанию SystemClock)
+	public TodoItem(string text, IClock clock = null)
 	{
+		_clock = clock ?? new SystemClock();
 		_text = text;
 		_status = TodoStatus.NotStarted;
-		_lastUpdate = DateTime.Now;
+		_lastUpdate = _clock.Now;
 	}
-	public TodoItem(string text, TodoStatus status, DateTime lastUpdate)
+
+	// Конструктор с указанием статуса и времени (для загрузки из файла)
+	public TodoItem(string text, TodoStatus status, DateTime lastUpdate, IClock clock = null)
 	{
+		_clock = clock ?? new SystemClock();
 		_text = text;
 		_status = status;
 		_lastUpdate = lastUpdate;
 	}
+
 	public bool GetIsDone()
 	{
 		return _status == TodoStatus.Completed;
 	}
+
 	public void MarkDone()
 	{
 		_status = TodoStatus.Completed;
-		_lastUpdate = DateTime.Now;
+		_lastUpdate = _clock.Now;
 	}
+
 	public void UpdateText(string newText)
 	{
 		_text = newText;
-		_lastUpdate = DateTime.Now;
+		_lastUpdate = _clock.Now;
 	}
+
 	public void UpdateStatus(TodoStatus newStatus)
 	{
 		_status = newStatus;
-		_lastUpdate = DateTime.Now;
+		_lastUpdate = _clock.Now;
 	}
+
 	public string GetShortInfo()
 	{
 		string shortText = _text.Length <= 30 ? _text : _text.Substring(0, 27) + "...";
 		return shortText;
 	}
+
 	public string GetFullInfo()
 	{
 		return $"Текст задачи: \n{_text}\nСтатус: {GetStatusText()}\nДата последнего изменения: {_lastUpdate:dd.MM.yyyy HH:mm:ss}";
 	}
+
 	public string GetText() => _text;
 	public TodoStatus GetStatus() => _status;
 	public DateTime GetLastUpdate() => _lastUpdate;
+
 	public string GetStatusText()
 	{
 		return _status switch
@@ -67,4 +84,5 @@ public class TodoItem
 			_ => "Неизвестно"
 		};
 	}
+}
 }
